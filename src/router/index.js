ef3e953aa086bcea9bbd21bea9bf63a9fbc20534
worldsong <2060884089@qq.com>
2017-10-18 17:50:01 +0800

    引入视图views

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import GoodsList from '@/views/GoodsList'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      name: 'GoodsList',
+      component: GoodsList
     }
   ]
 })
