4c47b2b56025c1de04de9e78d4b55f38acae195f
worldsong <2060884089@qq.com>
2017-10-19 16:17:53 +0800

    GoodsList组件拆分

---
+++ src/components/NavBread.vue
@@ -0,0 +1,28 @@
+/**
+* Created by Song on 2017/10/19.
+*/
+<template>
+    <div class="nav-breadcrumb-wrap">
+        <div class="container">
+            <nav class="nav-breadcrumb">
+                <a href="/">主页</a>
+                <span>商品列表</span>
+            </nav>
+        </div>
+    </div>
+</template>
+
+<script>
+    import './../assets/css/nav-bread.css'
+    export default {
+        data() {
+            return {}
+        },
+        components: {},
+        methods: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/NavFooter.vue
@@ -0,0 +1,42 @@
+/**
+* Created by Song on 2017/10/19.
+*/
+<template>
+    <div>
+        <footer class="footer">
+            <div class="footer__wrap">
+                <div class="footer__secondary">
+                    <div class="footer__inner">
+                        <div class="footer__secondary__nav">
+                            <span>Copyright©2013-2017 dongnaoedu.com All Rights Reserved.</span>
+                            <a href="https://dongnaoedu.com/">
+                                关于我们
+                            </a>
+                            <a href="http://dongnao.ke.qq.com/#tab=1&category=-1">
+                                腾讯课堂&&动脑学院
+                            </a>
+                            <a href="https://dongnaoedu.com/index.html">
+                                版权所有 2008-2017 湘ICP备16006411号-1
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script>
+    import './../assets/css/nav-footer.css'
+    export default {
+        data() {
+            return {}
+        },
+        components: {},
+        methods: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
