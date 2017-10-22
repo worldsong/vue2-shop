1224b5c85650922e640e0066c50435d9aeaf3da3
worldsong <2060884089@qq.com>
2017-10-22 15:21:12 +0800

    登录拦截

--- server/app.js
+++ server/app.js
@@ -25,6 +25,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req,res,next) {
+    if(req.cookies.userId){
+        next();
+    }else{
+        console.log("url:"+req.originalUrl);
+        if(req.originalUrl=='/users/login' || req.originalUrl=='/users/logout' || req.originalUrl.indexOf('/goods/list')>-1){
+            next();
+        }else{
+            res.json({
+                status:'10001',
+                msg:'当前未登录',
+                result:''
+            });
+        }
+    }
+});
+
 app.use('/', index);
 app.use('/goods', goods);
 app.use('/users', users);
