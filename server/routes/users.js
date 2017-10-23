371a1edd5abc1036aa4ab8c784080ba958c73796
worldsong <2060884089@qq.com>
2017-10-23 17:18:56 +0800

    订单确认模块前后端实现

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+require('./../util/util')
 var User = require('./../models/user');
 
 // 登录接口
@@ -270,5 +271,72 @@ router.post("/delAddress", function (req,res,next) {
             });
         }
     });
+});
+
+//
+router.post("/payMent", function (req,res,next) {
+    var userId = req.cookies.userId,
+        addressId = req.body.addressId,
+        orderTotal = req.body.orderTotal;
+    User.findOne({userId:userId}, function (err,doc) {
+        if(err){
+            res.json({
+                status:"1",
+                msg:err.message,
+                result:''
+            });
+        }else{
+            var address = '',goodsList = [];
+            //获取当前用户的地址信息
+            doc.addressList.forEach((item)=>{
+                if(addressId==item.addressId){
+                    address = item;
+                }
+            })
+            //获取用户购物车的购买商品
+            doc.cartList.filter((item)=>{
+                if(item.checked=='1'){
+                    goodsList.push(item);
+                }
+            });
+
+            var platform = '622';
+            var r1 = Math.floor(Math.random()*10);
+            var r2 = Math.floor(Math.random()*10);
+
+            var sysDate = new Date().Format('yyyyMMddhhmmss');
+            var createDate = new Date().Format('yyyy-MM-dd hh:mm:ss');
+            var orderId = platform+r1+sysDate+r2;
+            var order = {
+                orderId:orderId,
+                orderTotal:orderTotal,
+                addressInfo:address,
+                goodsList:goodsList,
+                orderStatus:'1',
+                createDate:createDate
+            };
+
+            doc.orderList.push(order);
+
+            doc.save(function (err1,doc1) {
+                if(err1){
+                    res.json({
+                        status:"1",
+                        msg:err.message,
+                        result:''
+                    });
+                }else{
+                    res.json({
+                        status:"0",
+                        msg:'',
+                        result:{
+                            orderId:order.orderId,
+                            orderTotal:order.orderTotal
+                        }
+                    });
+                }
+            });
+        }
+    })
 });
 module.exports = router;
