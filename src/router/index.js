f278ffa581f81fc7807acd4e91e9379c77eb8232
worldsong <2060884089@qq.com>
2017-10-23 17:31:36 +0800

    订单成功页面初始化

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import GoodsList from '@/views/GoodsList'
 import Cart from '@/views/Cart'
 import Address from '@/views/Address'
 import OrderConfirm from '@/views/OrderConfirm'
+import OrderSuccess from '@/views/OrderSuccess'
 
 
 Vue.use(Router)
@@ -29,6 +30,11 @@ export default new Router({
           path: '/orderConfirm',
           name: 'OrderConfirm',
           component:OrderConfirm
+      },
+      {
+          path: '/OrderSuccess',
+          name: 'OrderSuccess',
+          component:OrderSuccess
       },
   ]
 })
---
+++ src/views/OrderSuccess.vue
@@ -0,0 +1,22 @@
+/**
+* Created by Song on 2017/10/23.
+*/
+<template>
+    <div>{{msg}}</div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                msg: "hello vue"
+            }
+        },
+        components: {},
+        methods: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
